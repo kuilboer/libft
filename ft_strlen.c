71efe76c48cff24291f430d85e9778315c8a763c
Olaf <okuilboe@student.codam.nl>
2025-05-01 20:54:33 +0200

    updated ft_strlen() to be of type size_t instead of unsigned int.

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,16 +6,18 @@
 /*   By: okuilboe <okuilboe@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/25 12:45:19 by okuilboe      #+#    #+#                 */
-/*   Updated: 2025/04/28 17:33:41 by okuilboe      ########   odam.nl         */
+/*   Updated: 2025/05/01 20:51:28 by okuilboe      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlen(const char *s)
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
 {
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
 	while (s[i])
 		i++;
-	return (i);
+	return ((size_t)i);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: okuilboe <okuilboe@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/25 12:29:15 by okuilboe      #+#    #+#                 */
-/*   Updated: 2025/05/01 15:27:55 by okuilboe      ########   odam.nl         */
+/*   Updated: 2025/05/01 20:47:24 by okuilboe      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@
 
 #include <stddef.h>
 
-int				ft_isalnum(int c);
-int				ft_isalpha(int c);
-int				ft_isdigit(int c);
-int				ft_isascii(int c);
-int				ft_isprint(int c);
-int 			ft_tolower(int c);
-int				ft_toupper(int c);
-unsigned int	ft_strlen(const char *s);
-char 			*ft_strchr(const char *s, int c);
-char 			*ft_strrchr(const char *s, int c);
-int				ft_strncmp(const char *s1, const char *s2, size_t n);
-char			*ft_strnstr(const char *big, const char *little, size_t len);
-size_t			ft_strlcpy(char *dst, const char *src, size_t size);
-void 			*ft_memset(void *s, int c, unsigned int n);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int 	ft_tolower(int c);
+int		ft_toupper(int c);
+size_t	ft_strlen(const char *s);
+char 	*ft_strchr(const char *s, int c);
+char 	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+void 	*ft_memset(void *s, int c, unsigned int n);
 
 /*
 ft_memset
