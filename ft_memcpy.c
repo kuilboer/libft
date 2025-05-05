fed9aebf9af991e93e93dca9130b2e6a0296d0d4
Olaf <okuilboe@student.codam.nl>
2025-05-05 13:45:42 +0200

    added a NULL pointer safety check.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,11 @@
 /*   By: okuilboe <okuilboe@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/04 19:48:52 by okuilboe      #+#    #+#                 */
-/*   Updated: 2025/05/05 13:34:46 by okuilboe      ########   odam.nl         */
+/*   Updated: 2025/05/05 13:42:52 by okuilboe      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
@@ -19,6 +18,8 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*d_buf;
 	unsigned char	*s_buf;
 
+	if (!dest && !src)
+		return (NULL);
 	d_buf = (unsigned char *)dest;
 	s_buf = (unsigned char *)src;
 	i = 0;
