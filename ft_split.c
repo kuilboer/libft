451da8e5bc20548c5bb64ef6b2b57f7f3d5251d7
Olaf <okuilboe@student.codam.nl>
2025-05-16 14:17:50 +0200

    Reworked ft_split to fit the 25 line per function limit.

--- ft_split.c
+++ ft_split.c
@@ -6,120 +6,103 @@
 /*   By: okuilboe <okuilboe@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/08 15:07:34 by okuilboe      #+#    #+#                 */
-/*   Updated: 2025/05/11 20:02:32 by okuilboe      ########   odam.nl         */
+/*   Updated: 2025/05/16 13:48:04 by okuilboe      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-static void	clear_mem(char **arr, t_word *strc_arr)
+static int	count_words(const char *s, char c)
 {
-	int	i;
-
-	i = 0;
-	if (arr)
-	{
-		while (arr[i])
-			free(arr[i++]);
-		free(arr);
-	}
-	if (strc_arr)
-		free(strc_arr);
-	return ;
-}
-
-static int	word_count(const char *s, char delim)
-{
-	int	i;
-	int	count;
+	int		count;
+	int		in_word;
+	int		i;
 
 	count = 0;
+	in_word = 0;
 	i = 0;
 	while (s[i])
 	{
-		while (s[i] && s[i] == delim)
-			i++;
-		if (s[i] && s[i] != delim)
+		if (s[i] != c && in_word == 0)
 		{
+			in_word = 1;
 			count++;
-			while (s[i] && s[i] != delim)
-				i++;
 		}
+		else if (s[i] == c)
+			in_word = 0;
+		i++;
 	}
 	return (count);
 }
 
-static int	find_words(t_word *s_words, const char *s, char c)
+static void	free_words(char **arr, int filled)
 {
-	int		in_word;
-	size_t	word_i;
-	size_t	i;
+	int		i;
 
-	in_word = 0;
-	word_i = 0;
 	i = 0;
-	while (s[i] || in_word == 1)
-	{
-		if (s[i] != c && in_word == 0)
-		{
-			s_words[word_i].start_i = i;
-			s_words[word_i].end_i = 0;
-			s_words[word_i].len = 1;
-			in_word = 1;
-		}
-		else if ((s[i] == c || !s[i]) && in_word == 1)
-		{
-			s_words[word_i].end_i = i;
-			s_words[word_i].len = i - s_words[word_i].start_i;
-			word_i++;
-			in_word = 0;
-		}
-		if (!s[i])
-			break ;
-		i++;
-	}
-	return (word_i);
+	while (i < filled)
+		free(arr[i++]);
+	free(arr);
 }
 
-static int	copy_words(char **new_strs,	const char *s,
-	t_word *s_words,	size_t count)
+static char	*alloc_word(const char *s, int len)
 {
-	size_t	i;
+	char	*word;
+	int		i;
 
+	word = malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
 	i = 0;
-	while (i < count)
+	while (i < len)
 	{
-		new_strs[i] = (char *)malloc(sizeof(char) * (s_words[i].len + 1));
-		if (!new_strs[i])
-			clear_mem(new_strs, s_words);
-		ft_memcpy(new_strs[i], &s[s_words[i].start_i], s_words[i].len);
-		new_strs[i][s_words[i].len] = '\0';
+		word[i] = s[i];
 		i++;
 	}
-	new_strs[count] = NULL;
-	return (i);
+	word[i] = '\0';
+	return (word);
+}
+
+static int	fill_split(char **res, const char *s, char c)
+{
+	int		i;
+	int		len;
+	int		word_i;
+
+	i = 0;
+	word_i = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		len = 0;
+		while (s[i + len] && s[i + len] != c)
+			len++;
+		if (len > 0)
+		{
+			res[word_i] = alloc_word(&s[i], len);
+			if (!res[word_i])
+				return (free_words(res, word_i), 0);
+			word_i++;
+			i += len;
+		}
+	}
+	res[word_i] = NULL;
+	return (1);
 }
 
 char	**ft_split(const char *s, char c)
 {
-	char	**new_strs;
-	t_word	*s_words;
-	size_t	count;
+	char	**result;
+	int		count;
 
-	if (s == NULL )
+	if (!s)
 		return (NULL);
-	count = word_count(s, c);
-	new_strs = malloc((count + 1) * sizeof(char *));
-	s_words = malloc(count * sizeof(t_word));
-	if (!new_strs || !s_words)
-	{
-		clear_mem(new_strs, s_words);
+	count = count_words(s, c);
+	result = malloc(sizeof(char *) * (count + 1));
+	if (!result)
 		return (NULL);
-	}
-	find_words(s_words, s, c);
-	copy_words(new_strs, s, s_words, count);
-	free(s_words);
-	return (new_strs);
+	if (!fill_split(result, s, c))
+		return (NULL);
+	return (result);
 }
